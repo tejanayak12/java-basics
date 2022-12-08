00eb7b1406d8cfabbae011ab8cd09b389a52211c
Teja nayak <tejanayak12>
2022-12-08 11:51:38 +0530

    java basics

---
+++ 1-strings.js
@@ -0,0 +1,11 @@
+"name : Teja Nayak" 
+
+'teja nayak'
+
+`city : guntur`
+
+"name : teja \n city : guntur"
+
+`${5+5}` // 10 
+
+`${2+2}` // 4 
---
+++ 3-boolean.js
@@ -0,0 +1,9 @@
+TEja == TEja // true;
+
+siri == siri // true;
+
+teja == siri // false;
+
+10 == 10 // true;
+
+10 == 52 // false;
