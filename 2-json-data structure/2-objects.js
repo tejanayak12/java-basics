0917a34c32981c5738ed8fa9679224e733d17e48
Teja nayak <tejanayak12>
2022-12-10 10:45:15 +0530

    data-structur

---
+++ 2-json-data structure/2-objects.js
@@ -0,0 +1,13 @@
+({
+
+    "Name" : "Teja",
+    "City" : "Guntur",
+    "Designation" : "UI Developer",
+    "Company" : "SAP",
+
+    "Skills" : ["html","css","java"],
+    "Friends" :["harish","ramesh","kishore"]
+    
+
+
+})
