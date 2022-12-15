05787ed62b1e41a34330252d9f45da1841ca6dcc
Teja nayak <tejanayak12>
2022-12-15 18:08:36 +0530

    program added

---
+++ 4-myfirstprogram.js/index.js
@@ -0,0 +1,28 @@
+var intial = "jatavth";            
+let firstname = "teja";
+let lastname = "nayak";
+
+const city = "guntur";
+
+const friends =[
+    "harish",
+    "ramesh",
+    "satish"
+]
+const myDetails = {
+    fullname : `${jatavath},${teja},${nayak}`,
+    designation : "UI DEV",
+    city : city,
+    friends : friends
+}
+
+console.log(myDetails)
+
+
+// Javascript Runtime Environment
+// 1. Browser
+// 2. Node
+
+// Run this file in NODE environment
+// example: node FILENAME
+// node index.js
