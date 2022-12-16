c2fb4942c61adb2af1e5d64365d452f1578229fd
Teja nayak <tejanayak12>
2022-12-16 16:47:48 +0530

    input-output-added

---
+++ 5-input-output/index.js
@@ -0,0 +1,20 @@
+const readline = require("readline-sync");
+
+console.log("tablecaluculator");
+
+const table = readline.question("which number u want to caluculate = ");
+
+console.log(`${table} *5 = ${table*5}`);
+
+
+
+
+//const readline = require('readline-sync');
+
+// Output welcome message
+//console.log("Welcome to input and output program");
+
+// Ask user name from the input question
+//const username = readline.question("Please specify your name: ");
+
+//console.log(`Hello ${username}, Welcome to our program`)
