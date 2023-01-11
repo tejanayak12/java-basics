adbca7664e7100aff2618fd4d3c9d17afc99b41f
Teja nayak <tejanayak12>
2023-01-11 21:58:33 +0530

    task added

---
+++ 10-Loops/4-Nested-loop.js
@@ -0,0 +1,23 @@
+
+const table = 5;
+let start = 10;
+let end = 1;
+
+while( end  <= start){
+    console.log(`${table} x ${start} = ${ table * start}`);
+    start--
+};
+let startTable = 1;
+let endTable = 12;
+
+while(startTable <= endTable) {
+    console.log(` The following table is ${startTable}`)
+      let startNumber = 1;
+      const endNumber = 10;
+      while(startNumber <= endNumber) {
+        console.log(`${startTable} x ${startNumber} = ${ startTable * startNumber }`);
+        startNumber++;
+      }
+      console.log("----------------------------------------------------")
+      startTable++;
+    };
---
+++ 10-Loops/5-for-loop.js
@@ -0,0 +1,20 @@
+
+// let start = 10;  // loop initial phase
+// while( start <= 20){
+//     console.log( ` let the value be ${start}`);
+//     start++
+// }
+
+
+// for (  let i=10 ; i<=20 ; i++){
+//     console.log(i)
+// }
+for ( let i=1 ; i<=10 ; i++){
+    console.log(`The value of the table is ${i}`);
+
+    for ( let x=1 ; x<=10  ;x++){
+        console.log(` ${i} x ${x} = ${i * x}`);
+    
+    }
+    console.log(`-------------------`);
+}
