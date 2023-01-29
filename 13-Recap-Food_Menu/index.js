74cf9bcc22db5c6aded65e8eba8a6f04cfcd8b5a
Teja nayak <tejanayak12>
2023-01-29 11:30:40 +0530

    part 2 added

--- 13-Recap-Food_Menu/index.js
+++ 13-Recap-Food_Menu/index.js
@@ -1,7 +1,11 @@
 import chalk from "chalk";
 import readline from 'readline-sync';
-import { categories } from "./utils.js"
+import { getcategory,getproductinfo,displayproductinfo } from "./utils.js"
 
 console.log(chalk.bgGreen("Welcome to our shopping app"));
+const selectedcategory = getcategory();
 
-readline.keyInSelect(categories);
+if (selectedcategory){
+    const product = getproductinfo(selectedcategory);
+    displayproductinfo(product)
+};
--- 13-Recap-Food_Menu/utils.js
+++ 13-Recap-Food_Menu/utils.js
@@ -1,4 +1,6 @@
+import chalk from 'chalk';
 import fileSystem from 'fs';
+import readline from 'readline-sync';
 
 function getFileData(filePath) {
   const fileBufferData = fileSystem.readFileSync(filePath);
@@ -15,4 +17,39 @@ function getFileData(filePath) {
 // console.log(products)
 
 export const categories = getFileData('./data/categories.json');
-const products = getFileData("./data/products.json");
+export const products = getFileData("./data/products.json");
+
+export function getcategory() {
+  const displaycategory = categories.map((category) => {
+    const firstname = category[0].toUpperCase();
+    const remaining = category.substr(1).toLowerCase();
+    return firstname + remaining
+  });
+
+  const userselectedindex = readline.keyInSelect(displaycategory);
+  console.log(`user selected index is `, userselectedindex);
+  const userselectedcategory = categories[userselectedindex];
+  console.log(`user selected category`, userselectedcategory);
+  return userselectedcategory
+};
+
+export function getproductinfo(category) {
+  if (category) {
+    const categoryproducts = products.filter(Product => Product.category === category);
+    const productsnames = categoryproducts.map(product => product.title);
+    const selectedproduct = readline.keyInSelect(productsnames);
+    const selectedone = categoryproducts[selectedproduct];
+    return selectedone
+  }
+};
+
+export function displayproductinfo (product){
+  if(product){
+    const id = product.id;
+    const title = product.title;
+    const price = product.price;
+    const category = product.category;
+    console.log(chalk.bgRed(`product title `,title) , `id was `,id);
+    console.log(chalk.bgGreen(`price is = `,price) , `category was = `,category);
+  }
+};
